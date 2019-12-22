6e11f416d7620ce917987969e56123f5f1718a2d
negovan <negivr@yahoo.com>
2019-12-22 01:54:09 -0600

    sredjen title u base_person.html

--- dq/views.py
+++ dq/views.py
@@ -5,6 +5,7 @@ from .models import Person
 def home(request):
     persons = Person.objects.all()
     context = {
+        'title': 'people',
         'persons': persons
     }
     return render(request, "dq/person_list.html", context)
